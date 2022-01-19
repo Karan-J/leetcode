e4c8a2ad756c9c6abd1257bacf749ed6712a036d
Karan Jariwala <karanjariwala@Karans-Air.attlocal.net>
2022-01-18 22:55:18 -0600

    Function to print nth Fibonacci number

---
+++ leetcode-2022/Fibonacci.py
@@ -0,0 +1,13 @@
+def fib( n: int) -> int:
+	if n < 2:
+            return n
+        
+        first = 0
+        second = 1
+        
+        for i in range(2,n+1):
+            third = first + second
+            first = second
+            second = third
+            
+        return second
