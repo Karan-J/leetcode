62fdc5c0fe62791b3e08fd8c073a0118472d3e06
Karan-J <karanja1296@gmail.com>
2022-01-25 23:01:54 -0600

    Updated the code a bit

--- leetcode-2022/Intersection of Two Linked Lists.py
+++ leetcode-2022/Intersection of Two Linked Lists.py
@@ -20,6 +20,7 @@ def getIntersectionNode(headA, headB) -> [ListNode]:
     
     tempB = headB
     while tempB is not None:
+        # print(tempB.val, tempB in setA,setA)
         if tempB in setA:
             return tempB
         tempB = tempB.next
@@ -29,5 +30,10 @@ def getIntersectionNode(headA, headB) -> [ListNode]:
 if __name__ == '__main__':
     headA = ListNode(1)
     headA.next = ListNode(3)
+    headA.next.next = ListNode(2)
+#   need to add the nodes properly using add method of linkedlist for the code to work
+    
     headB = ListNode(3)
+    headB.next = ListNode(2)
+    print(headA.val, headA.next.val, headB.val)
     print(getIntersectionNode(headA,headB))
