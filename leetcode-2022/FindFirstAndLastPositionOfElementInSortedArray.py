9958c331118ee948f41b973c0d2595cc52d7c6b7
Karan-J <karanja1296@gmail.com>
2022-03-30 21:30:29 -0500

    Added finding range of element in a sorted array with updated code

--- leetcode-2022/FindFirstAndLastPositionOfElementInSortedArray.py
+++ leetcode-2022/FindFirstAndLastPositionOfElementInSortedArray.py
@@ -23,50 +23,54 @@ nums is a non-decreasing array.
 
 '''
 
-# def searchRange(nums, target):
+def searchRange(nums, target):
         
-#     lower_bound = findBound(nums, target, True)
-#     if (lower_bound == -1):
-#         return [-1, -1]
+    lower_bound = findBound(nums, target, True)
+    if (lower_bound == -1):
+        return [-1, -1]
     
-#     upper_bound = findBound(nums, target, False)
-#     return [lower_bound, upper_bound]
+    upper_bound = findBound(nums, target, False)
+    return [lower_bound, upper_bound]
         
 
 
-# def findBound(nums, target, isFirst):
+def findBound(nums, target, isFirst):
     
-#     N = len(nums)
-#     begin = 0 
-#     end = N - 1
-#     while begin <= end:
-#         mid = end - (begin + end) // 2   
+    N = len(nums)
+    begin = 0 
+    end = N - 1
+
+    if len(nums) == 0:
+        return -1
+
+    while begin <= end:
+        mid = begin + (end - begin) // 2   
         
-#         if nums[mid] == target:
+        if nums[mid] == target:
             
-#             if isFirst:
-#                 # This means we found our lower bound.
-#                 if mid == begin or nums[mid - 1] < target:
-#                     return mid
+            if isFirst:
+                # This means we found our lower bound.
+                if mid == begin or nums[mid - 1] < target:
+                    return mid
 
-#                 # Search on the left side for the bound.
-#                 end = mid - 1
+                # Search on the left side for the bound.
+                end = mid - 1
                 
-#             else:
+            else:
                 
-#                 # This means we found our upper bound.
-#                 if mid == end or nums[mid + 1] > target:
-#                     return mid
+                # This means we found our upper bound.
+                if mid == end or nums[mid + 1] > target:
+                    return mid
                 
-#                 # Search on the right side for the bound.
-#                 begin = mid + 1
+                # Search on the right side for the bound.
+                begin = mid + 1
         
-#         elif nums[mid] > target:
-#             end = mid - 1
-#         else:
-#             begin = mid + 1
+        elif nums[mid] > target:
+            end = mid - 1
+        else:
+            begin = mid + 1
     
-#     return -1
+    return -1
 
 
 def search(nums, target):
@@ -113,12 +117,12 @@ if __name__ == '__main__':
     nums = [5,7,7,8,8,10]
     target = 8
     print(search(nums,target))
-    # print(searchRange(nums,target))
+    print(searchRange(nums,target))
     
     target = 6
-    # print(searchRange(nums,target))
+    print(searchRange(nums,target))
     print(search(nums,target))
     nums = []
     target = 0
-    # print(searchRange(nums,target))
+    print(searchRange(nums,target))
     print(search(nums,target))
