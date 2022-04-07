f67bfdebe6fba6e8f9608c8851862c97d474bc43
Karan-J <karanja1296@gmail.com>
2022-04-07 00:45:33 -0500

    Added intersection of two arrays with multiple returns

---
+++ leetcode-2022/IntersectionOfTwoArrays2.py
@@ -0,0 +1,56 @@
+'''
+Given two integer arrays nums1 and nums2, return an array of their intersection. 
+Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+ 
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+ 
+
+Constraints:
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+ 
+
+Follow up:
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+'''
+
+
+def intersect(nums1,nums2):
+    if len(nums1) > len(nums2):
+        return intersect(nums2,nums1)
+
+    d = {}
+    for i in nums1:
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] += 1
+
+    out = []
+    for j in nums2:
+        if j in d and d[j] > 0:
+            out.append(j)
+            d[j] -= 1
+    
+    return out 
+
+
+if __name__ == '__main__':
+    nums1 = [1,2,2,1]
+    nums2 = [2,2]
+    print(intersect(nums1,nums2))
+    nums1 = [4,9,5]
+    nums2 = [9,4,9,8,4]
+    print(intersect(nums1,nums2))
