8e6ae506842a790fa72bff915b9d93d932fb3166
Karan-J <karanja1296@gmail.com>
2022-04-13 23:25:23 -0500

    Added the iterative DP solution

--- leetcode-2022/LongestCommonSubsequence.py
+++ leetcode-2022/LongestCommonSubsequence.py
@@ -52,13 +52,25 @@ def longestCommonSubsequence(text1, text2):
     return memo_solve(0, 0)
 
 
+def lcs(text1, text2):
+    dp_table = [[0]*(len(text2) + 1) for _ in range(len(text1) + 1)]
+    for col in reversed(range(len(text2))):
+        for row in reversed(range(len(text1))):
+            if text1[row] == text2[col]:
+                dp_table[row][col] = 1 + dp_table[row + 1][col + 1]
+            else:
+                dp_table[row][col] = max( dp_table[row][col + 1], dp_table[row + 1][col] )
+    
+    return dp_table[0][0]
+
+
 if __name__ == '__main__':
     text1 = "abcde"
     text2 = "ace" 
-    print(longestCommonSubsequence(text1,text2))
+    print(longestCommonSubsequence(text1,text2), lcs(text1,text2))
     text1 = "abc"
     text2 = "abc"
-    print(longestCommonSubsequence(text1,text2))
+    print(longestCommonSubsequence(text1,text2), lcs(text1,text2))
     text1 = "abc"
     text2 = "def"
-    print(longestCommonSubsequence(text1,text2))
+    print(longestCommonSubsequence(text1,text2), lcs(text1,text2))
